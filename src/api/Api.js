58035c5b62936a120999e3e64e834d2d818c120d
reidacarne <71790865+reidacarne@users.noreply.github.com>
2020-09-28 19:00:35 -0300

    função randomica adicionada

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://picsum.photos/v2',
-    timeout: 1000
+    baseURL: 'https://picsum.photos/'
   });
 
 export default api;
